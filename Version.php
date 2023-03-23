c4a5198966d35ae0c4f8206250c90a21b696886b
Leonardo Gomes Barreiro <Leo.barreiro@hotmail.com>
2023-03-23 09:57:24 -0300

    Update Version.php

--- Version.php
+++ Version.php
@@ -2,8 +2,16 @@
 
 defined('MOODLE_INTERNAL') || die;
 
+function xmldb_local_sincroniza_plugin_install() {
+    global $DB;
+    require_once($DB->dir . '/local/sincroniza_plugin/db/install.php');
+    xmldb_local_sincroniza_plugin_install();
+}
+
 $plugin->version   = 2023030101;
 $plugin->requires  = 2020110900;
 $plugin->component = 'local_sincroniza_plugin';
 $plugin->release   = '1.0.0';
 $plugin->maturity  = MATURITY_STABLE;
+
+?>
